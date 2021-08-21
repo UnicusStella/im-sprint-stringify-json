e992994cbb00900920ea7ea5e76a882b7ac24999
UnicusStella <unicusstella@gmail.com>
2021-08-21 18:49:02 +0900

    Initial State

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,10 +1,10 @@
 /**
- * 1. Broswer에 존재하는 JSON.stringfy 함수를 직접구현해봅니다.
+ * 1. Browser에 존재하는 JSON.stringfy 함수를 직접 구현해 봅니다.
  * JSON.stringfy 함수는 input 값을 JSON 형식으로 변환합니다.
- * 단, undefind와 function은 JSON으로 생략되거나 null 로 변환됩니다.
+ * 단, undefined와 function은 JSON으로 생략되거나 null로 변환됩니다.
  *
  * 2. stringfyJSON은 아래와 같이 작동합니다.
- * - Bolean이 input으로 주어졌을 경우
+ * - Boolean이 input으로 주어졌을 경우
  * stringifyJSON(true);                // 'true'
  * - String이 input으로 주어졌을 경우
  * stringifyJSON('foo');               // '"foo"'
@@ -12,57 +12,24 @@
  * stringifyJSON([1, 'false', false]); // '[1,"false",false]'
  * - Object가 input으로 주어졌을 경우
  * stringifyJSON({ x: 5 });            // '{"x":5}'
- * - undefind, function이 주어졌을 경우
+ * - undefined, function이 주어졌을 경우
  * stringifyJSON(undefined)            // undefined
  * stringifyJSON(function(){})         // undefined
  * stringifyJSON({ x: undefined, y: function(){} })   // '{}'
  *
- * 3. spec/fixtures.js를 참고해서 테스트에서 어떤 input 값들이
- * 주어지고, 어떻게 stringify해 주어야할지 생각해 보세요.
+ * 3. spec/stringifyJSONSpec.js의 stringifiableObjects 배열을 참고해서 테스트에서 어떤 input 값들이
+ * 주어지고, 어떻게 stringify해 주어야 할지 생각해 보세요.
  *
- * 4. 그냥 테스트 통과를 하고 싶으시다면, 다음과 같이 구현하면 될거예요.
+ * 4. 그냥 테스트 통과를 하고 싶으시다면, 다음과 같이 구현하면 될 거예요.
  *  const stringifyJSON = JSON.stringify;
  *
  * 하지만 이 과제의 목적은 재귀를 공부하는 것이니, 처음부터 구현해봐야겠지요?:
  */
-
 function stringifyJSON(obj) {
   // your code goes here
-  if (obj === null) {
-    return 'null';
-  }
-
-  if (typeof obj === 'number' || typeof obj === 'boolean') {
-    return `${obj}`;
-  }
-
-  if (typeof obj === 'string') {
-    return `"${obj}"`;
-  }
-
-  // obj가 배열인 경우...
-  if (Array.isArray(obj)) {
-    let newArr = [];
-
-    for (let el of obj) {
-      newArr.push(stringifyJSON(el));
-    }
-    return `[${newArr}]`;
-  }
-  if (typeof obj === 'object') {
-    let result = '';
-    for (let key in obj) {
-      if (obj[key] === undefined || typeof obj[key] === 'function') {
-        result = String(result);
-      } else {
-        result = result + `${stringifyJSON(key)}:${stringifyJSON(obj[key])},`;
-      }
-    }
-    result = result.slice(0, result.length - 1);
-    return `{${result}}`;
-  }
 }
-// 다음 코드는 결과 제출을 위한 코드입니다. 신경쓰지 않아도 좋습니다.
+
+// 다음 코드는 결과 제출을 위한 코드입니다. 신경 쓰지 않아도 좋습니다.
 if (typeof window === 'undefined') {
   module.exports = stringifyJSON;
 }
